f3f93e8c9cb66451abeb678b39b39ab9da644bb0
md-naim-molla <naim.molla.stats@gmail.com>
2026-04-17 12:37:34 +0600

    something happens

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find, find_packages
+from setuptools import setup, find_packages
 
 setup(
-    name = 'us_visa',
-    version = '0.0.0',
-    author= 'Nayeem',
-    author_email= 'nayeem24125@gmail.com',
-    packages= find_packages()
+    name='us_visa',
+    version='0.0.0',
+    author='Nayeem',
+    author_email='nayeem24125@gmail.com',
+    packages=find_packages(),
 )
